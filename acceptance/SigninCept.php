eb678143f8a3a6542b12b4e52793d074c045936a
nimestel <nimestell@gmail.com>
2018-03-20 19:50:36 +0300

    более удобная структура кода

--- acceptance/SigninCept.php
+++ acceptance/SigninCept.php
@@ -4,11 +4,11 @@ $I->am('user'); // роль исполнителя
 $I->wantTo('login to website'); // что проверяется
 $I->lookForwardTo('access website features for logged-in users'); // ожидаемый результат
 $I->amOnPage('/');
-//вызов формы входа
+//переход на форму входа
+$I->seeLink('Войти');
 $I->click('#myModal a'); 
 //ввод логина и пароля
-//$I->submitForm('loginform', ['userlogin' => 'testme', 'userpassword' => '123']);
 $I->fillField('userlogin','testme');
 $I->fillField('userpassword','123');
 $I->click('Войти', ['class' => 'btn--auth-submit']); // нажать на кнопку Войти
-$I->See('Здравствуйте, ');
+$I->see('Здравствуйте, ', '.auth-user__hello'); //увидеть приветствие пользователя
