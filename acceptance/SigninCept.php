63ca0c8c0c7aeeaa2df3c1e2cabc2dcc492ab612
nimestel <nimestell@gmail.com>
2018-03-20 12:14:09 +0300

    доработка log in

--- acceptance/SigninCept.php
+++ acceptance/SigninCept.php
@@ -4,7 +4,9 @@ $I->am('user'); // actor's role
 $I->wantTo('login to website'); // feature to test
 $I->lookForwardTo('access website features for logged-in users'); // result to achieve
 $I->amOnPage('/');
-$I->click('Войти');
+$I->click('#myModal a');
+//$I->submitForm('loginform', ['userlogin' => 'testme', 'userpassword' => '123']);
 $I->fillField('userlogin','testme');
 $I->fillField('userpassword','123');
-$I->click('Войти');
+$I->click(['class' => 'btn--auth-submit']);
+$I->canSee('Здравствуйте, ');
